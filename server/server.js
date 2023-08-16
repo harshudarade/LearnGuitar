14e891fb29578822ee777ea9fbdc04715c1b0aa6
harshudarade <hbdarade2018@gmail.com>
2023-08-16 16:30:21 +0530

    update

--- server/server.js
+++ server/server.js
@@ -40,15 +40,15 @@ connectDB();
 
 // Serve frontend
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '.../frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '.../frontend/build')));
 
-    app.get('*', (req, res) => res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build' , 'index.html')
-    )
-    );
-} else {
-    app.get('/', (req, res) => res.send('Please set to production'));
-}
+//     app.get('*', (req, res) => res.sendFile(
+//         path.resolve(__dirname, '../', 'frontend', 'build' , 'index.html')
+//     )
+//     );
+// } else {
+//     app.get('/', (req, res) => res.send('Please set to production'));
+// }
 
 // https://www.youtube.com/watch?v=gWIbT1fbLlA&list=LL&index=1&ab_channel=IndianCodersz
